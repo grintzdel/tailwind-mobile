b326d27bfb4b2eedd019a5ce2297e11e42bca1ea
maoudin <mathis.oudin42@gmail.com>
2025-10-23 23:28:20 +0200

    feat: add header and footer

---
+++ src/ui/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer: React.FC = (): React.JSX.Element => {
+    return (
+        <footer>
+            <h1>hello from the footer</h1>
+        </footer>
+    )
+}
---
+++ src/ui/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header: React.FC = (): React.JSX.Element => {
+    return (
+        <header>
+            <h1>hello from the header</h1>
+        </header>
+    )
+}
