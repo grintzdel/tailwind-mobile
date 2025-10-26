c5a131a3d86ad818f9cfd57a9da723015bb76ef5
maoudin <mathis.oudin42@gmail.com>
2025-10-26 22:32:48 +0100

    feat: add generic shared song list section component

---
+++ src/modules/shared/react/components/song-list-section.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import {SongTile} from "@/modules/song/react/components/song-tile";
+import {SongDomainModel} from "@/modules/song/core/model/song.domain-model";
+
+type DisplayMode = 'tag' | 'artist';
+
+export type SongItem = {
+    img: string;
+    title: string;
+    tag?: SongDomainModel.Tag;
+    artist?: string;
+}
+
+type SongListSectionProps = {
+    songs: SongItem[];
+    displayMode: DisplayMode;
+}
+
+export const SongListSection: React.FC<SongListSectionProps> = ({
+                                                                    songs,
+                                                                    displayMode
+                                                                }: SongListSectionProps): React.JSX.Element => {
+    return (
+        <div className="flex flex-col gap-3 px-5">
+            {songs.map((song, index) => (
+                <SongTile
+                    key={index}
+                    img={song.img}
+                    title={song.title}
+                    tag={song.tag}
+                    artist={song.artist}
+                    displayMode={displayMode}
+                />
+            ))}
+        </div>
+    );
+};
