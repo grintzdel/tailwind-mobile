c38c3496adebe14266dcd02ff45916322e4e3c90
maoudin <mathis.oudin42@gmail.com>
2025-10-26 16:04:08 +0100

    feat: update categories section

--- src/modules/app/react/components/categories-section.tsx
+++ src/modules/app/react/components/categories-section.tsx
@@ -1,9 +1,25 @@
+'use client';
+
 import React from "react";
+
+import {Button} from "@/ui/button";
+import {ScrollableHorizontalContainer} from "@/modules/app/react/components/components/scrollable-horizontal-container";
+
+const voidOnClick = (): void => {
+    return
+}
 
 export const CategoriesSection: React.FC = (): React.JSX.Element => {
     return (
-        <section>
-            <h1>Hello from the categories section !</h1>
+        <section className="flex flex-col gap-4 py-8">
+            <h1 className="px-5 font-bold text-3xl">Hello, Alex</h1>
+            <ScrollableHorizontalContainer>
+                <Button text="All" onClick={voidOnClick} variant="primary"/>
+                <Button text="New Artist" onClick={voidOnClick} variant="primary"/>
+                <Button text="Hot Tracks" onClick={voidOnClick} variant="primary"/>
+                <Button text="Editor's Picks" onClick={voidOnClick} variant="primary"/>
+                <Button text="Random Button" onClick={voidOnClick} variant="primary"/>
+            </ScrollableHorizontalContainer>
         </section>
     )
 }
