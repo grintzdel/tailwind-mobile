cfc11505d8c548e286ddda87d447df443ec440d1
maoudin <mathis.oudin42@gmail.com>
2025-10-27 00:16:54 +0100

    fix: icons size

--- src/modules/song/react/layout/song-header.tsx
+++ src/modules/song/react/layout/song-header.tsx
@@ -10,10 +10,10 @@ import {ButtonIcon} from "@/ui/button";
 export const SongHeader: React.FC = (): React.JSX.Element => {
     return (
         <div className="flex flex-row justify-between items-center gap-3 p-5">
-            <ButtonIcon icon={IoIosArrowBack} href="/library" variant="lg"/>
+            <ButtonIcon icon={IoIosArrowBack} href="/library" variant="lg" className="h-7 w-8"/>
             <h1>Now Playing</h1>
             <ButtonIcon icon={RiShareBoxFill} onClick={() => {
-            }} variant="lg"/>
+            }} variant="lg" className="h-7 w-8"/>
         </div>
     );
 };
