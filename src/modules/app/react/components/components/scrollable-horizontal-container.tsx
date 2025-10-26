9f4e9b93e2823380c37444a7dee0d95cf2b97e82
maoudin <mathis.oudin42@gmail.com>
2025-10-26 16:03:52 +0100

    feat: add scrollable horizontal container for categories section

---
+++ src/modules/app/react/components/components/scrollable-horizontal-container.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type ScrollableHorizontalContainerProps = {
+    children: React.ReactNode
+}
+
+export const ScrollableHorizontalContainer: React.FC<ScrollableHorizontalContainerProps> = (
+    {children}: ScrollableHorizontalContainerProps): React.JSX.Element => {
+    return (
+        <div
+            className="flex flex-row gap-3 overflow-x-auto w-full scroll-smooth snap-mandatory overscroll-contain px-5">
+            {children}
+        </div>
+    )
+}
