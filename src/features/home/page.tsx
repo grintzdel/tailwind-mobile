90958b934033d0098653d0d73e537638ddafef0e
maoudin <mathis.oudin42@gmail.com>
2025-10-26 16:06:01 +0100

    fix: rename files and path

--- src/features/home/page.tsx
+++ src/features/home/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import {CategoriesSection} from "@/modules/app/react/components/categories-section";
-import {ForYouSection} from "@/modules/app/react/components/for-you-section";
-import {PopularSection} from "@/modules/app/react/components/popular-section";
+import {CategoriesSection} from "@/modules/app/react/components/categories.section";
+import {ForYouSection} from "@/modules/app/react/components/for-you.section";
+import {PopularSection} from "@/modules/app/react/components/popular.section";
 
 export default function HomePage() {
     return (
