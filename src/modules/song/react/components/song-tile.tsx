1757ed76e7977eddc68911503309fe07c054748c
maoudin <mathis.oudin42@gmail.com>
2025-10-26 22:30:20 +0100

    feat: add display mode props in song tile

--- src/modules/song/react/components/song-tile.tsx
+++ src/modules/song/react/components/song-tile.tsx
@@ -5,16 +5,27 @@ import Image from "next/image";
 import {MdPlayArrow} from "react-icons/md";
 
 import {SongDomainModel} from "@/modules/song/core/model/song.domain-model";
-
 import {IconWithCircle} from "@/ui/icon";
+
+type DisplayMode = 'tag' | 'artist';
 
 type SongTileProps = {
     img: string;
     title: string;
-    tag: SongDomainModel.Tag;
+    tag?: SongDomainModel.Tag;
+    artist?: string;
+    displayMode: DisplayMode;
 }
 
-export const SongTile: React.FC<SongTileProps> = ({img, title, tag}: SongTileProps): React.JSX.Element => {
+export const SongTile: React.FC<SongTileProps> = ({
+                                                      img,
+                                                      title,
+                                                      tag,
+                                                      artist,
+                                                      displayMode
+                                                  }: SongTileProps): React.JSX.Element => {
+    const subtitle = displayMode === 'tag' ? tag : artist;
+
     return (
         <div className="flex flex-row gap-4 justify-between items-center">
             <div className="flex flex-row gap-3 items-center content-center">
@@ -24,10 +35,10 @@ export const SongTile: React.FC<SongTileProps> = ({img, title, tag}: SongTilePro
                 </div>
                 <div className="flex flex-col">
                     <h3 className="font-medium">{title}</h3>
-                    <p className="font-light text-white/70 text-sm capitalize">{tag}</p>
+                    <p className="font-light text-white/70 text-sm capitalize">{subtitle}</p>
                 </div>
             </div>
-            <IconWithCircle icon={MdPlayArrow} className="h-7 w-8"/>
+            <IconWithCircle icon={MdPlayArrow} className="h-7 w-8" variant="sm"/>
         </div>
     )
 }
