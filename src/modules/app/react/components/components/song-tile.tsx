34d5834b04da7a69868c7ed9c51e98c579bb80d1
maoudin <mathis.oudin42@gmail.com>
2025-10-26 18:23:12 +0100

    feat: add song-tile component

---
+++ src/modules/app/react/components/components/song-tile.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Image from "next/image";
+
+import {MdPlayArrow} from "react-icons/md";
+
+import {SongDomainModel} from "@/modules/song/core/model/song.domain-model";
+
+import {IconWithCircle} from "@/ui/icon";
+
+type SongTileProps = {
+    img: string;
+    title: string;
+    tag: SongDomainModel.Tag;
+}
+
+export const SongTile: React.FC<SongTileProps> = ({img, title, tag}: SongTileProps): React.JSX.Element => {
+    return (
+        <div className="flex flex-row gap-4 justify-between items-center">
+            <div className="flex flex-row gap-3 items-center content-center">
+                <div className="rounded-xl h-[60px] w-[60px] flex-shrink-0">
+                    <Image src={img} alt={img} height={60} width={60}
+                           className="rounded-xl object-cover h-full w-full"/>
+                </div>
+                <div className="flex flex-col">
+                    <h3 className="font-medium">{title}</h3>
+                    <p className="font-light text-white/70 text-sm">{tag}</p>
+                </div>
+            </div>
+            <IconWithCircle icon={MdPlayArrow} className="h-7 w-8"/>
+        </div>
+    )
+}
