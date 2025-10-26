3189d1a00a5144f311e25add46b2b12d24e1b278
maoudin <mathis.oudin42@gmail.com>
2025-10-26 16:43:44 +0100

    feat: update for you section

--- src/modules/app/react/components/for-you.section.tsx
+++ src/modules/app/react/components/for-you.section.tsx
@@ -1,9 +1,22 @@
+'use client'
+
 import React from "react";
+
+import {ScrollableHorizontalContainer} from "@/modules/app/react/components/components/scrollable-horizontal-container";
+import {Card} from "@/ui/card";
 
 export const ForYouSection: React.FC = (): React.JSX.Element => {
     return (
-        <section>
-            <h1>hello from the for you section !</h1>
+        <section className="flex flex-col gap-3">
+            <h2 className="px-5 font-bold text-xl">For you</h2>
+            <ScrollableHorizontalContainer>
+                <Card title="Feel the Beat" subtitle="Explore trending tracks and hidden gems curated just for you."
+                      href="#" bgImage="/card-1-media.jpg"/>
+                <Card title="Feel the Beat" subtitle="Explore trending tracks and hidden gems curated just for you."
+                      href="#" bgImage="/card-1-media.jpg"/>
+                <Card title="Feel the Beat" subtitle="Explore trending tracks and hidden gems curated just for you."
+                      href="#" bgImage="/card-1-media.jpg"/>
+            </ScrollableHorizontalContainer>
         </section>
     )
 }
