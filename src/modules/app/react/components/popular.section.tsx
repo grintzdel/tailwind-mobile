d34f86ea4a1adcdf1759d3af9463b81e6997497b
maoudin <mathis.oudin42@gmail.com>
2025-10-26 18:23:32 +0100

    feat: update popular section

--- src/modules/app/react/components/popular.section.tsx
+++ src/modules/app/react/components/popular.section.tsx
@@ -5,13 +5,21 @@ import React from "react";
 import {Section} from "@/ui/section";
 import {Button} from "@/ui/button";
 
+import {SongTile} from "@/modules/app/react/components/components/song-tile";
+
 export const PopularSection: React.FC = (): React.JSX.Element => {
     return (
         <Section
             title="Popular"
-            headerAction={<Button text="Show all" variant="secondary" onClick={() => {}} />}
+            headerAction={<Button text="Show all" variant="secondary" onClick={() => {
+            }}/>}
         >
-            {/* TODO: Add popular content here */}
+            <div className="flex flex-col gap-3 px-5">
+                <SongTile img="/popular-1.jpg" title="Blinding Light" tag="Top Hit"/>
+                <SongTile img="/popular-2.jpg" title="Ocean Eyes" tag="Soft Vibe"/>
+                <SongTile img="/popular-3.jpg" title="Circle Run" tag="Fan Fav"/>
+                <SongTile img="/popular-1.jpg" title="Random Song" tag="Random Song"/>
+            </div>
         </Section>
     )
 }
