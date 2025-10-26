427bd6796fcaa23756b7dc05072127bf6f041a18
maoudin <mathis.oudin42@gmail.com>
2025-10-26 16:01:02 +0100

    fix: lint and fix padding

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+
+import {CiSearch} from "react-icons/ci";
+import {IoMdNotificationsOutline} from "react-icons/io";
+
 import {Avatar} from "@/ui/avatar";
 import {IconWithCircle} from "@/ui/icon";
-import { CiSearch } from "react-icons/ci";
-import { IoMdNotificationsOutline } from "react-icons/io";
 
 export const Header: React.FC = (): React.JSX.Element => {
     return (
-        <header className="flex flex-row justify-between p-7">
-            <Avatar src="/header-avatar.jpg" />
+        <header className="flex flex-row justify-between px-5 py-5">
+            <Avatar src="/header-avatar.jpg"/>
             <div className="flex flex-row gap-2">
-                <IconWithCircle icon={CiSearch} className="h-4 w-4" />
-                <IconWithCircle icon={IoMdNotificationsOutline} className="h-4 w-4" />
+                <IconWithCircle icon={CiSearch} className="h-4 w-4"/>
+                <IconWithCircle icon={IoMdNotificationsOutline} className="h-4 w-4"/>
             </div>
         </header>
     )
